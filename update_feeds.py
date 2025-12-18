c0af7a76c88c61305028188be6a550bd37424f71
avrianpradiptya <93908849+avrianpradiptya@users.noreply.github.com>
2025-12-18 08:56:10 +0700

    Add more RSS feeds to update_feeds.py

--- update_feeds.py
+++ update_feeds.py
@@ -2,11 +2,16 @@ import feedparser, json, datetime
 
 # List of RSS feeds
 feeds = {
-    "detik.com": "https://news.detik.com/berita/rss",
-    "antaranews.com": "https://www.antaranews.com/rss/top-news",
-    "kompas.com": "https://rss.kompas.com/api/feed/social?apikey=bc58c81819dff4b8d5c53540a2fc7ffd83e6314a",
-    "tempo.co": "http://rss.tempo.co/nasional",
-    "cnnindonesia.com": "https://www.cnnindonesia.com/nasional/rss"
+"detik.com": "https://news.detik.com/berita/rss",
+"antaranews.com": "https://www.antaranews.com/rss/top-news",
+"kompas.com": "https://rss.kompas.com/api/feed/social?apikey=bc58c81819dff4b8d5c53540a2fc7ffd83e6314a",
+"tempo.co": "http://rss.tempo.co/nasional",
+"republika.co.id": "http://www.republika.co.id/rss"
+"viva.co": "https://www.viva.co.id/get/all"
+"jpnn.com": "https://www.jpnn.com/index.php?mib=rss"
+"times.co.id": "https://www.times.co.id/rss/latest-posts"
+"BBC Indonesia": "https://www.bbc.com/indonesia/berita_indonesia/index.xml"
+"antaranews Jogja": "https://jogja.antaranews.com/rss/terkini.xml"
 }
 
 results = []
@@ -27,3 +32,4 @@ with open("data/latest.json", "w", encoding="utf-8") as f:
     json.dump(data, f, ensure_ascii=False, indent=2)
 
 print("latest.json updated successfully inside /data folder!")
+
