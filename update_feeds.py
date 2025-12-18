3c3ae19bf812298f38cf0a79a3b0688137a7f58a
avrianpradiptya <93908849+avrianpradiptya@users.noreply.github.com>
2025-12-18 09:03:27 +0700

    Update update_feeds.py

--- update_feeds.py
+++ update_feeds.py
@@ -6,12 +6,12 @@ feeds = {
 "antaranews.com": "https://www.antaranews.com/rss/top-news",
 "kompas.com": "https://rss.kompas.com/api/feed/social?apikey=bc58c81819dff4b8d5c53540a2fc7ffd83e6314a",
 "tempo.co": "http://rss.tempo.co/nasional",
-"republika.co.id": "http://www.republika.co.id/rss"
-"viva.co": "https://www.viva.co.id/get/all"
-"jpnn.com": "https://www.jpnn.com/index.php?mib=rss"
-"times.co.id": "https://www.times.co.id/rss/latest-posts"
-"BBC Indonesia": "https://www.bbc.com/indonesia/berita_indonesia/index.xml"
-"antaranews Jogja": "https://jogja.antaranews.com/rss/terkini.xml"
+"republika.co.id": "http://www.republika.co.id/rss",
+"viva.co": "https://www.viva.co.id/get/all",
+"jpnn.com": "https://www.jpnn.com/index.php?mib=rss",
+"times.co.id": "https://www.times.co.id/rss/latest-posts",
+"BBC Indonesia": "https://www.bbc.com/indonesia/berita_indonesia/index.xml",
+"antaranews Jogja": "https://jogja.antaranews.com/rss/terkini.xml",
 }
 
 results = []
@@ -32,4 +32,5 @@ with open("data/latest.json", "w", encoding="utf-8") as f:
     json.dump(data, f, ensure_ascii=False, indent=2)
 
 print("latest.json updated successfully inside /data folder!")
+
 
